83c23e91a7bc5ae5ad8a1349c983f92021378912
Blessing <ayeniblessing32@gmail.com>
2017-06-07 23:44:00 +0100

    Add decision statement to constructor

--- coding_labs_1/src/car.js
+++ coding_labs_1/src/car.js
@@ -6,35 +6,34 @@ class Car{
     this.model = model;
     this.name = name;
     this.wheels = 4;
+    
+    if(name === undefined) {
+      this.name = 'General';
+    }
+    
+    if(model === undefined) {
+      this.name = 'GM';
+    }
+    
+    if(type === "Porshe" || type === "Koenigsegg")
+    {
+      this.doors = 2;
+    }
+    
+    if(name === "trailer")
+    {
+      this.wheels = 8;
+    }
+    
+    if(name === 'trailer')
+    {
+      this.speed = '0 km/h';
+    }
+    else
+    {
+      this.speed = '77 km/h'
+    }
   }
-
-  if(name === undefined) {
-    this.name = 'General';
-  }
-  
-  if(model === undefined) {
-    this.name = 'GM';
-  }
-  
-  if(type === "Porshe" || type === "Koenigsegg")
-  {
-    this.doors = 2;
-  }
-  
-  if(name === "trailer")
-  {
-    this.wheels = 8;
-  }
-  
-  if(name === 'trailer')
-  {
-    this.speed = '0 km/h';
-  }
-  else
-  {
-    this.speed = '77 km/h'
-  }
-  
 
   drive(number) {
     let man  = new Car('MAN', 'Truck', 'trailer');
